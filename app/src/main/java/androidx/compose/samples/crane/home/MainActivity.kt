0ff6f785ebf01ec75c565066edcf831b9a84eb91
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-08-24 23:44:50 +0530

    Minor Changes

--- app/src/main/java/androidx/compose/samples/crane/home/MainActivity.kt
+++ app/src/main/java/androidx/compose/samples/crane/home/MainActivity.kt
@@ -36,9 +36,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
         setContent {
             CraneTheme {
                 MainScreen(onExploreItemClicked = {
